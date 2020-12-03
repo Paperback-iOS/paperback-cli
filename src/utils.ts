7a7fb51fc563986b29d24cb24902d97b78964383
Faizan Durrani <faizandurrani324@gmail.com>
2020-12-03 22:25:53 +0500

    fixed log method not working in github actions

--- src/utils.ts
+++ src/utils.ts
@@ -15,7 +15,7 @@ export default class Utils {
   }
 
   static log(message = '') {
-    (process.stdout as any).cursorTo(0)
+    (process.stdout as any)?.cursorTo(0)
     process.stdout.write(this.prefixTime(message) + '\n')
   }
 
