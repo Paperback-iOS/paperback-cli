3cec3f69c68e025efa74c3106bbdb2c1a3d47479
Faizan Durrani <faizandurrani324@gmail.com>
2021-09-23 21:26:52 +0500

    Updated console styling

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -37,7 +37,9 @@ export default class Test extends CLICommand {
 
     let sourcesToTest: string[] = []
     if (sourceId) {
-      sourcesToTest = [sourceId]
+      sourcesToTest = fs.readdirSync(bundleDir).filter(file => {
+        return file.toLowerCase() === sourceId.toLowerCase() && fs.statSync(path.join(bundleDir, file)).isDirectory()
+      })
     } else {
       sourcesToTest = fs.readdirSync(bundleDir).filter(file => fs.statSync(path.join(bundleDir, file)).isDirectory())
     }
@@ -45,21 +47,27 @@ export default class Test extends CLICommand {
     if (flags.ip) {
       // connect to the app here
     } else {
+      console.clear()
       const tester = new SourceTester(bundleDir)
       for (const source of sourcesToTest) {
+        this.log()
+        this.log(chalk.bold.underline.bgBlue.white(`Testing ${source}`))
         const request = new SourceTestRequest()
         request.setSourceid(source)
         request.setData(new SourceTestRequest.TestData())
 
         await tester.testSource(request, async response => {
-          this.log()
           this.log(`${chalk.red.bold('#')} ${chalk.bold(response.getTestcase())}: ${chalk.green(response.getCompletetime().toFixed(0) + 'ms')}`)
 
-          response.getFailuresList().forEach(failure => {
-            this.log(`${chalk.bold.white.bgRed('[FAILURE]')} ${failure}`)
+          const failures = response.getFailuresList()
+          failures.forEach(failure => {
+            this.log(`- ${chalk.white.bgRed('[FAILURE]')} ${failure}`)
           })
+
+          if (failures.length > 0) this.log()
         })
       }
+      this.log()
     }
   }
 }
